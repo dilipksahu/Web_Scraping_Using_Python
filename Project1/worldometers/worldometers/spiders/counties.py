e1fd141f5b6e41e7b80edbf4261dcb74c46c9193
Dilip <sahud048@gmail.com>
2020-11-27 21:08:42 +0530

    Create new project for web scrap using scrapy

---
+++ Project1/worldometers/worldometers/spiders/counties.py
@@ -0,0 +1,36 @@
+import scrapy
+import logging
+
+
+class CountiesSpider(scrapy.Spider):
+    name = 'counties'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    country_name = ''
+
+    def parse(self, response):
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+            self.country_name = name
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+
+            # yield scrapy.Request(url=absolute_url)
+            # Relative url
+            yield response.follow(link, callback=self.parse_countries, meta={'country_name':name})
+
+    def parse_countries(self, response):
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'country_name' : name,
+                'year': year,
+                'population': population
+            }
