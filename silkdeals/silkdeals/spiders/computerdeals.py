a767c6ab296c90ddb99a51fd4a86b222597ed4d7
Dilip <sahud048@gmail.com>
2020-12-03 01:05:32 +0530

    Scrape multiple page data using scrapy-selenium

---
+++ silkdeals/silkdeals/spiders/computerdeals.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+
+class ComputerdealsSpider(scrapy.Spider):
+    name = 'computerdeals'
+    
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://slickdeals.net/computer-deals/',
+            wait_time=3,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        products = response.xpath("//ul[@class='dealTiles categoryGridDeals']/li")
+        for product in products:
+            yield {
+                'name': product.xpath(".//a[@class='itemTitle bp-p-dealLink bp-c-link']/text()").get(),
+                'url': product.xpath(".//a[@class='itemTitle bp-p-dealLink bp-c-link']/@href").get(),
+                'price': product.xpath("normalize-space(.//div[contains(@class,'itemPrice  wide ')]/text())").get(),
+            }
+
+        next_page = response.xpath('//a[@data-role="next-page"]/@href').get()
+        if next_page:
+            absolute_path = f"https://slickdeals.net{next_page}"
+            yield SeleniumRequest(
+                url=absolute_path,
+                wait_time=3,
+                callback= self.parse
+            )
